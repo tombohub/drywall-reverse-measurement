3f3d5f72267b64a1e18576e4ae19cfc49c67db5e
me <tkutlesa@gmail.com>
2024-01-09 16:05:20 -0500

    removed jotai

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,7 @@
-import { atom } from "jotai";
-import { randomRegularMeasurement } from "../drywall";
-
 import { configureStore } from "@reduxjs/toolkit";
 import questionReducer from "./questionSlice";
 import gameReducer from "./gameSlice";
 import countdownReducer from "./countdownSlice";
-import { calculateReverseMeasurement } from "../drywall";
 
 export const store = configureStore({
   reducer: {
@@ -16,17 +12,3 @@ export const store = configureStore({
 });
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-
-/**
- * current regular measurement given to the user to guess the * reverse measurement
- */
-export const regularMeasurementAtom = atom(randomRegularMeasurement());
-regularMeasurementAtom.debugLabel = "regular-measurement";
-
-/**
- * Correct reverse measurement for the current regular measurement.
- */
-export const reverseMeasurementAtom = atom(get =>
-  calculateReverseMeasurement(get(regularMeasurementAtom))
-);
-reverseMeasurementAtom.debugLabel = "reverse-measurement";
